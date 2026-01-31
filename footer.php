491bc9924ece67dd8bf16a5ec888327cfd7e3ced
nast <eha20082@gmail.com>
2026-01-31 12:56:42 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-  <footer>
-    <p>
-      © Koidulauliku E-laulik
-      <br>
-      <a href="/viitet.php">Viited & litsentsid</a>
-      <br>
-        <strong>Võta kontakt:</strong> asi2026elaulik@gmail.com
-    </p>
-  </footer>
